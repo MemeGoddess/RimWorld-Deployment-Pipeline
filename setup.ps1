a8adca4243781393c06153ae50c852a4def7f6a7
Lana Avery <memekoten@gmail.com>
2026-03-01 01:45:10 +1100

    Added project side of dependency management system

---
+++ setup.ps1
@@ -0,0 +1,38 @@
+function workshop {
+    if (Get-Command "steamcmd" -ErrorAction SilentlyContinue) {
+        Write-Host "steamcmd is installed."
+    } else {
+        Write-Host "steamcmd is not installed."
+        exit 1
+    }
+
+    $workshopItemsFile = "RequiredWorkshopItems.txt"
+    $workshopItems = ""
+
+    if (Test-Path $workshopItemsFile) {
+        $workshopItems = Get-Content $workshopItemsFile | Where-Object { $_ -ne "" } | ForEach-Object { "+workshop_download_item 294100 $_" }
+        Write-Host "Workshop items: '$workshopItems'"
+    } else {
+        Write-Host "RequiredWorkshopItems.txt file not found. Nothing to download."
+        exit 1
+    }
+
+    $scriptDirectory = $PSScriptRoot
+    $downloadPath = Join-Path $scriptDirectory "Workshop"
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -ItemType Directory -Path $downloadPath | Out-Null
+    }
+
+    &steamcmd +force_install_dir $downloadPath +login anonymous $workshopItems +quit
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to download workshop item ID: $itemId"
+        exit 1
+    }
+
+    Write-Host "Workshop items downloaded successfully."
+
+}
+
+workshop
+Write-Host "Setup complete. Solution should be buildable now."
